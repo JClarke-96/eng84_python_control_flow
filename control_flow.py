8a855c1d72b70df786a78499da03e53aba78e602
Your Name <you@example.com>
2021-03-23 15:35:14 +0000

    Control_flow.py

--- control_flow.py
+++ control_flow.py
@@ -3,11 +3,12 @@
 
 age = input("How old is the user?   ") # Ask age of the user
 
-if int(age) >= 18:   # If true, will print statement
+# Code to tell user which rating of tickets they can purchase
+if int(age) >= 18:      # User is older than 18 and can buy any ticket
     print("This user can purchase any ticket")
-elif int(age) >= 15:    # If true, will print statement
+elif int(age) >= 15:    # User is older than 15, but younger than 18
     print("This user can purchase tickets with a rating of U, PG, 12, or 15")
-elif int(age) >= 12:
+elif int(age) >= 12:    # User is older than 12, but younger than 15
     print("This user can purchase tickets with a rating of U, PG, or 12")
-else:
+else:                   # User is below the age of 12
     print("This user can only watch films with a rating of U or PG")
